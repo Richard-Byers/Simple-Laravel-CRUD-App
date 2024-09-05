8cdad69fc202f013c963421d2815472cd12e95ac
Richard <69055743+Richard-Byers@users.noreply.github.com>
2024-09-05 23:28:41 +0100

    Basic CRUD blog app completed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,6 +10,34 @@
     <form action="/logout" method="POST">
         @csrf
         <button>Log Out </button>
+    </form>
+
+    <div style="border: 3px solid black;">
+        <h2>Create a New Post</h2>
+        <form action="/create-post" method="POST">
+          @csrf
+          <input type="text" name="title" placeholder="post title">
+          <textarea name="body" placeholder="content"></textarea>
+          <button>Save Post</button>
+        </form>
+      </div>
+
+      <div style="border: 3px solid black;">
+        <h2>Posts</h2>
+        @foreach($posts as $post)
+        <div style="background-color: grey; padding: 10px; margin: 10px;">
+            <h3>{{$post['title']}} by {{$post->user->name}}</h3>
+            {{$post['body']}}
+            <p><a href="/edit-post/{{$post->id}}" method="POST">Edit</a></p>
+            <form action = "/delete-post/{{$post->id}}" method="POST">
+                @csrf
+                @method('DELETE')
+                <button>Delete</button>
+            </form>
+        </div>
+        @endforeach
+      </div>
+
     @else
     <div style="border: 3px solid black;">
     <h2>Register</h2>
